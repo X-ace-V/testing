298d5bb69b3d0014ae5444876996477b86691346
X-ace-V <kumarvibhanshu21@gmail.com>
2024-09-25 19:35:53 +0530

    testing on aws

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.json({
+        message: "Hello World"
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server started at Port 3000!');
+})
